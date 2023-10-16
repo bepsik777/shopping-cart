6b2e94cea292a0fa4aefac0696e2c9efffb9ba56
bepsik777 <potocki.alex@gmail.com>
2023-10-16 15:59:59 +0200

    Change navigation bar layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="px-5 py-8 bg-pink-300 flex justify-around items-center h-1/6">
+    <header className="px-5 py-8 bg-pink-300 flex justify-end items-center h-1/6">
       <div></div>
-      <div className="w-1/3 flex justify-between">
-        <Link to="/">Home</Link>
-        <Link to="shop">Shop</Link>
+      <nav className="w-4/6 flex justify-between px-2">
+        <div className="w-1/2 flex justify-around">
+          <Link to="/">Home</Link>
+          <Link to="shop">Shop</Link>
+        </div>
         <Link to="cart">Cart</Link>
-      </div>
+      </nav>
       <div></div>
     </header>
   );
