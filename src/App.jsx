a84171a4f8d4c3f25a3dd90d50133c626c0c073b
bepsik777 <potocki.alex@gmail.com>
2023-10-17 15:02:11 +0200

    Change Header and Shop styling and breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 
 function App() {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
   return (
     <div className="bg-blue-300 min-h-screen flex flex-col">
             <Header></Header>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,11 +3,12 @@ const Card = ({ item }) => {
       <div>
         <img src={item.url ? item.url : ""} alt="" className="w-60 h-72 border-black border-2"/>
         <p className="w-60 min-h-max border-black border-2 p-4">{item.description}</p>
-        <div className="w-60 flex justify-between">
-            <span>+</span>
+        <div className="w-60 flex justify-between mt-3">
+            <button>+</button>
             <input type="number" />
-            <span>-</span>
+            <button>-</button>
         </div>
+        <button className="w-full mt-2 bg-slate-400 px-2 py-1 hover:bg-slate-500 hover:text-white">Add to cart</button>
       </div>
   );
 };
@@ -89,7 +90,7 @@ const Shop = () => {
       },
   ];
   return (
-    <main className="bg-red-400 p-4 grid grid-cols-3 gap-6 place-items-center">
+    <main className="bg-red-400 p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
       {fakeObjectAray.map((item) => {
         return <Card item={item} key={1} />;
       })}
