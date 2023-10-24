f8cb919f7ab3698c454cc4a12aa43e5a6e94d45c
bepsik777 <potocki.alex@gmail.com>
2023-10-24 17:38:18 +0200

    Add prop types validation to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'
 
 const Header = ({list}) => {
 
@@ -32,5 +33,11 @@ const Header = ({list}) => {
     </header>
   );
 };
+
+Header.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.object
+  )
+}
 
 export default Header;
