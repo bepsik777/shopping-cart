c727f7e3a122810953ecdebeb270ef77f0bc20e6
bepsik777 <potocki.alex@gmail.com>
2023-10-29 23:57:14 +0100

    First crazy experiment commit

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,45 +1,47 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
 import Router from "../components/Router";
 
 describe("App component", () => {
-  it("Renders correctly", () => {
+  it.only("Renders correctly", async () => {
     render(<App />, { wrapper: Router });
-    const home = screen.getByText(/I am Home/i);
+    const home = await screen.getByText(/I am Home/i).toBeInTheDocument();
     expect(home).toBeInTheDocument();
   });
 
   it("Renders shop when clicked on shop link", async () => {
-    render(<App/>, {wrapper: Router});
-    const user = userEvent.setup()
-    const link = screen.getByRole("link", {name: "Shop"})
-    
-    await user.click(link);
+    render(<App />, { wrapper: Router });
+    const user = userEvent.setup();
 
-    expect(screen.getByText(/This is shop/i)).toBeInTheDocument()
-  })
+    await waitFor(() =>
+      expect(screen.getByText(/I am Home/i)).toBeInTheDocument()
+    );
+
+    const link = screen.getByRole("link", { name: "Shop" });
+    await user.click(link);
+    expect(screen.getByText("title1")).toBeInTheDocument();
+  });
 
   it("Render cart when clicked on cart link", async () => {
-    render(<App/>, {wrapper: Router});
-    const user = userEvent.setup()
-    const link = screen.getByRole("link", {name: "Cart"})
+    render(<App />, { wrapper: Router });
+    const user = userEvent.setup();
+    const link = screen.getByRole("link", { name: "Cart" });
 
-    await user.click(link)
+    await user.click(link);
 
-    expect(screen.getByText(/No item in cart yet/i)).toBeInTheDocument()
-  })
+    expect(screen.getByText(/No item in cart yet/i)).toBeInTheDocument();
+  });
 
   it("Render home when clicked on home link from another path", async () => {
-    render(<App/>, {wrapper: Router});
-    const user = userEvent.setup()
-    const linkOne = screen.getByRole("link", {name: "Home"})
-    const linkTwo = screen.getByRole("link", {name: "Cart"})
+    render(<App />, { wrapper: Router });
+    const user = userEvent.setup();
+    const linkOne = screen.getByRole("link", { name: "Home" });
+    const linkTwo = screen.getByRole("link", { name: "Cart" });
 
-
-    await user.click(linkTwo)
-    await user.click(linkOne)
-    expect(screen.getByText(/I am Home/i)).toBeInTheDocument()
-  })
+    await user.click(linkTwo);
+    await user.click(linkOne);
+    expect(screen.getByText(/I am Home/i)).toBeInTheDocument();
+  });
 });
--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,49 +1,17 @@
-import { afterAll, afterEach, beforeAll } from 'vitest'
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
-import {setupServer} from 'msw/node'
-import {http} from 'msw'
+import { afterAll, afterEach, beforeAll, vi } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import { server } from "../mocks/server";
 
 
-const mockData = [
-  {
-    id: 1,
-    title: 'titleOne',
-    price: 10,
-  },
-  {
-    id: 2,
-    title: 'titleTwo',
-    price: 10,
-  },
-  {
-    id: 3,
-    title: 'titleThree',
-    price: 10,
-  },
-  {
-    id: 4,
-    title: 'titleFour',
-    price: 10,
-  },
-]
 
-export const restHandlers = [
-  http.get("https://fakestoreapi.com/products", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(mockData))
-  })
-]
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
-const server = setupServer(...restHandlers)
+afterAll(() => server.close());
 
-beforeAll(() => server.listen({onUnhandledRequest: 'error'}))
-
-afterAll(() => server.close())
-
-// afterEach(() => server.resetHandlers())
+afterEach(() => server.resetHandlers());
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
-  server.resetHandlers()
 });
